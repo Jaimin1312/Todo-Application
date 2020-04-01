4dec37ec509ae8ece59360fb619e3a26fda1b6a2
Jaimin1312 <61569981+Jaimin1312@users.noreply.github.com>
2020-04-01 23:33:13 +0530

    Update todo.sql

--- todo.sql
+++ todo.sql
@@ -34,34 +34,6 @@ CREATE TABLE `todo` (
   `date` datetime NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `todo`
---
-
-INSERT INTO `todo` (`id`, `title`, `description`, `status`, `date`) VALUES
-(22, 'c++ Programming', 'c++ is programming language', 'Pending', '2020-04-01 11:24:47'),
-(21, 'Today Work', 'Java Learning', 'Working', '2020-04-01 11:09:09'),
-(19, 'Today Work', 'Spring Boot Learning', 'Working', '2020-03-26 09:56:09');
-
---
--- Indexes for dumped tables
+-`
 --
 
---
--- Indexes for table `todo`
---
-ALTER TABLE `todo`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `todo`
---
-ALTER TABLE `todo`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
