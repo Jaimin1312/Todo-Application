1248d6a0c5b19256f08fc0140109804cdbfd256f
Jaimin1312 <61569981+Jaimin1312@users.noreply.github.com>
2020-04-01 23:33:56 +0530

    Update todo.sql

--- todo.sql
+++ todo.sql
@@ -1,30 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 01, 2020 at 05:59 PM
--- Server version: 5.7.14
--- PHP Version: 5.6.25
+Database: `todoapp`
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `todoapp`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `todo`
---
 
 CREATE TABLE `todo` (
   `id` int(255) NOT NULL,
@@ -33,7 +8,4 @@ CREATE TABLE `todo` (
   `status` varchar(255) NOT NULL,
   `date` datetime NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--`
---
 
